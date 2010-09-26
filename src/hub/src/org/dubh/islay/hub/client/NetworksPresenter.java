9293bd3bccb665c3da92c601e686c04a034888bf
Brian Duff <bduff@bduff-macbookpro.local>
2010-09-25 23:37:36 -0700

    Move more implementation into the presenter.

--- src/hub/src/org/dubh/islay/hub/client/NetworksPresenter.java
+++ src/hub/src/org/dubh/islay/hub/client/NetworksPresenter.java
@@ -4,6 +4,11 @@ import org.dubh.islay.hub.model.NetworkAssociation;
 import org.dubh.islay.hub.model.UserAccount;
 import org.dubh.islay.hub.shared.Network;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.EventBus;
 import com.gwtplatform.mvp.client.Presenter;
@@ -11,6 +16,7 @@ import com.gwtplatform.mvp.client.View;
 import com.gwtplatform.mvp.client.annotations.NameToken;
 import com.gwtplatform.mvp.client.annotations.ProxyStandard;
 import com.gwtplatform.mvp.client.proxy.PlaceManager;
+import com.gwtplatform.mvp.client.proxy.PlaceRequest;
 import com.gwtplatform.mvp.client.proxy.ProxyPlace;
 import com.gwtplatform.mvp.client.proxy.RevealRootContentEvent;
 
@@ -19,13 +25,26 @@ public class NetworksPresenter extends Presenter<NetworksPresenter.MyView, Netwo
   public static final String TOKEN = "networks";
   
   private final PlaceManager placeManager;
-  
+  private final NetworkAuthServiceAsync authService;
+  private final UserAccountServiceAsync userService;
+
   private UserAccount currentUser;
+  private String oauthVerifier;
+  private String oauthToken;
   
   @Inject
-  NetworksPresenter(EventBus eventBus, MyView view, MyProxy proxy, PlaceManager placeManager) {
+  NetworksPresenter(EventBus eventBus, MyView view, MyProxy proxy, PlaceManager placeManager,
+      NetworkAuthServiceAsync authService, UserAccountServiceAsync userService) {
     super(eventBus, view, proxy);
     this.placeManager = placeManager;
+    this.authService = authService;
+    this.userService = userService;
+  }
+
+  @Override
+  public void prepareFromRequest(PlaceRequest request) {
+    oauthVerifier = request.getParameter("oauth_verifier", null);
+    oauthToken = request.getParameter("oauth_token", null);
   }
   
   @Override
@@ -38,9 +57,63 @@ public class NetworksPresenter extends Presenter<NetworksPresenter.MyView, Netwo
       return;
     }
     
+    getView().button().addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(ClickEvent e) {
+        authService.getRequestTokenUrl(Network.BUZZ, new AsyncCallback<String>() {
+          @Override
+          public void onFailure(Throwable caught) {
+            caught.printStackTrace();
+            getView().showMessage("An error occurred.");
+          }
+
+          @Override
+          public void onSuccess(String result) {
+            // Redirect to the authorization page for this service. It should eventually
+            // call us back.
+            Window.Location.assign(result);
+          }
+        });
+      }
+    });
+    
     if (checkForSuccessfulAssociation()) {
       return;
     }
+    
+    if (oauthVerifier != null && oauthToken != null) {
+      getView().setButtonEnabled(false);
+      getView().showMessage("Requesting authorization...");
+      authService.getAccessToken(Network.BUZZ, oauthToken, oauthVerifier, new AsyncCallback<Void>() {
+        @Override
+        public void onFailure(Throwable caught) {
+          getView().showMessage("Failed to get authorization to Buzz :(");
+        }
+
+        @Override
+        public void onSuccess(Void result) {
+          // Cool.. call back the user service to refresh the current user, and 
+          // make sure it worked.
+          // TODO(bduff) we should use EventBus for this kind of thing. 
+          updateUser();
+        }
+      });
+    }
+  }
+  
+  private void updateUser() {
+    userService.getLoggedInUser(new AsyncCallback<UserAccount>() {
+      @Override
+      public void onFailure(Throwable caught) {
+        getView().showMessage("Failed to get current user");
+      }
+
+      @Override
+      public void onSuccess(UserAccount result) {
+        UserLoggedInEvent.fire(NetworksPresenter.this, currentUser);
+        checkForSuccessfulAssociation();
+      }
+    });
   }
   
   private boolean checkForSuccessfulAssociation() {
@@ -61,6 +134,7 @@ public class NetworksPresenter extends Presenter<NetworksPresenter.MyView, Netwo
   public interface MyView extends View {
     void setButtonEnabled(boolean enabled);
     void showMessage(String message);
+    HasClickHandlers button();
   }
   
   @ProxyStandard

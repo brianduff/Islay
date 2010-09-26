b6f3c85232992b575b89fee349c6ecf8a26a7ad4
Dominique Poudret <dominique.poudret@gmail.com>
2010-09-25 23:41:46 -0700

    Slight Refactoring in NetworksPresenter, RegistrationPresenter as well as adding registration for NetworksPresenter

--- src/hub/src/org/dubh/islay/hub/client/NetworksPresenter.java
+++ src/hub/src/org/dubh/islay/hub/client/NetworksPresenter.java
@@ -1,50 +1,45 @@
 package org.dubh.islay.hub.client;
 
 import org.dubh.islay.hub.model.NetworkAssociation;
-import org.dubh.islay.hub.model.UserAccount;
 import org.dubh.islay.hub.shared.Network;
 
 import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.EventBus;
-import com.gwtplatform.mvp.client.Presenter;
 import com.gwtplatform.mvp.client.View;
 import com.gwtplatform.mvp.client.annotations.NameToken;
+import com.gwtplatform.mvp.client.annotations.ProxyEvent;
 import com.gwtplatform.mvp.client.annotations.ProxyStandard;
 import com.gwtplatform.mvp.client.proxy.PlaceManager;
 import com.gwtplatform.mvp.client.proxy.ProxyPlace;
-import com.gwtplatform.mvp.client.proxy.RevealRootContentEvent;
 
-public class NetworksPresenter extends Presenter<NetworksPresenter.MyView, NetworksPresenter.MyProxy>
-    implements UserLoggedInEvent.Handler {
+
+
+/**
+ * 
+ * @author Islandir
+ *
+ */
+public class NetworksPresenter extends AuthenticatedUserPresenter<NetworksPresenter.MyView, NetworksPresenter.MyProxy> {
   public static final String TOKEN = "networks";
   
-  private final PlaceManager placeManager;
-  
-  private UserAccount currentUser;
   
   @Inject
   NetworksPresenter(EventBus eventBus, MyView view, MyProxy proxy, PlaceManager placeManager) {
-    super(eventBus, view, proxy);
-    this.placeManager = placeManager;
+    super(eventBus, view, proxy, placeManager);
   }
   
   @Override
-  protected void onBind() {
-    super.onBind();
-    if (currentUser == null) {
-      // Perhaps the user came straight here, bypassing the login page. Punt them
-      // back to the login page.
-      placeManager.revealDefaultPlace();
-      return;
-    }
-    
-    if (checkForSuccessfulAssociation()) {
-      return;
-    }
+  protected void onReveal() {
+    super.onReveal();
+  }
+
+  @Override
+  protected void postReveal() {
+    checkForSuccessfulAssociation();
   }
   
   private boolean checkForSuccessfulAssociation() {
-    NetworkAssociation assoc = currentUser.getNetworkAssociation(Network.BUZZ);
+    NetworkAssociation assoc = getCurrentUser().getNetworkAssociation(Network.BUZZ);
     if (assoc != null && assoc.isAccessTokenGranted()) {
       getView().setButtonEnabled(false);
       getView().showMessage("Successfully authorized to the Buzz API!");
@@ -53,22 +48,18 @@ public class NetworksPresenter extends Presenter<NetworksPresenter.MyView, Netwo
     return false;
   }
   
-  @Override
-  protected void revealInParent() {
-    RevealRootContentEvent.fire(this, this);
-  }
-  
   public interface MyView extends View {
     void setButtonEnabled(boolean enabled);
     void showMessage(String message);
   }
-  
+
+  @ProxyEvent
+  @Override
+  public void onUserLoggedIn(UserLoggedInEvent event) {
+    super.onUserLoggedIn(event);
+  }
+
   @ProxyStandard
   @NameToken(TOKEN)
   public interface MyProxy extends ProxyPlace<NetworksPresenter> {}
-
-  @Override
-  public void onUserLoggedIn(UserLoggedInEvent event) {
-    currentUser = event.getUserAccount();
-  }
 }

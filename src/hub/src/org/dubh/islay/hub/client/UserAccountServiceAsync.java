f1a8867d92da324de7bb6235ab6ef271247b9961
Brian Duff <cairndubh@gmail.com>
2010-10-01 01:26:11 -0700

    Use memcache to cache the currently logged in user (avoids going to the datastore). Don't know if this really speeds anything up significantly, (probably not a great deal, since we go out of our way to avoid requesting the current user over the wire anyway, and try to keep it cached in the client). May speed up refreshes a little?
    Anyway, just wanted to try out the memcache API, which I've singularly
    managed to completely avoid using so far at work (hehe.. maybe that's
    why our app runs so slowly :} ).

--- src/hub/src/org/dubh/islay/hub/client/UserAccountServiceAsync.java
+++ src/hub/src/org/dubh/islay/hub/client/UserAccountServiceAsync.java
@@ -6,7 +6,5 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface UserAccountServiceAsync {
   void getLoggedInUser(AsyncCallback<UserAccount> callback);
-  void createLoginUrl(AsyncCallback<String> callback);
-  void createLogoutUrl(AsyncCallback<String> callback);
   void save(UserAccount account, AsyncCallback<Void> callback);
 }
